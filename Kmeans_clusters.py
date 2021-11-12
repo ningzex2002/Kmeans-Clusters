f51a0b5b369aa35c6e5119355e1f42f9e5283812
ningzex2002 <91176015+ningzex2002@users.noreply.github.com>
2021-11-11 23:40:44 -0800

    Update Kmeans_clusters.py

--- Kmeans_clusters.py
+++ Kmeans_clusters.py
@@ -9,16 +9,16 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import altair as alt
-import sklearn as sk
+import sklearn
 
 
 st.title("Kmeans Clusters")
 max_iter = st.slider("Choose the number of iterations",1)
 
-X, _ = sk.datasets.make_blobs(n_samples=1000, centers=5, n_features=2, random_state = 1)
+X, _ = sklearn.datasets.make_blobs(n_samples=1000, centers=5, n_features=2, random_state = 1)
 df = pd.DataFrame(X, columns = list("ab"))
 starting_points = np.array([[0,0],[-2,0],[-4,0],[0,2],[0,4]])
-kmeans = sk.cluster.KMeans(n_clusters = 5, max_iter=max_iter, init=starting_points, n_init = 1)
+kmeans = sklearn.cluster.KMeans(n_clusters = 5, max_iter=max_iter, init=starting_points, n_init = 1)
 kmeans.fit(X);
 df["c"] = kmeans.predict(X)
 chart1 = alt.Chart(df).mark_circle().encode(
